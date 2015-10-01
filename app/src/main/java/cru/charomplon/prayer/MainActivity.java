8c66238182787fea9f552f576654877e33cb8a2e
bosszaa <charomplon01@gmial.com>
2015-10-01 10:23:25 +0700

    Start Project

---
+++ app/src/main/java/cru/charomplon/prayer/MainActivity.java
@@ -0,0 +1,13 @@
+package cru.charomplon.prayer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
